467743da3feab7f02bd98735c0c936ccd3bd5216
lisadw10 <80140031+lisadw10@users.noreply.github.com>
2021-11-16 10:59:41 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -25,28 +25,28 @@ while True:
                 print('==Treatment Anda==')
                 for list_nota in range(0, len(nota)):
                     print(f'{list_nota + 1}. {nota[list_nota]}')
-                    ulangi = False
+                ulangi = False
                 continue
     elif Treatment == '2' :
         for Treatment_Bodycare in range(0, len(list_Bodycare)): 
             print(f'{Treatment_Bodycare + 1}. {list_Bodycare[Treatment_Bodycare]} ')
-            ulangi = True
-            while ulangi :
-                Bodycare = int(input(f'Silahkan Pilih Treatment 1-{len(list_Bodycare)} '))
-                if Bodycare <= 0 or Bodycare > len(list_Bodycare) :
-                    print("====================")
-                    for Treatment_Bodycare in range(0, len(list_Bodycare)) :
-                        print(f'{Treatment_Bodycare + 1}. {list_Bodycare[Treatment_Bodycare]} ')
-                    continue
-                else:
-                    nota.append(list_Bodycare[Bodycare - 1])
-                    print('==Treatment Anda')
-                    for list_nota in range(0, len(nota)):
-                        print(f'{list_nota + 1}. {nota[list_nota]}')
-                    ulangi = False
+        ulangi = True
+        while ulangi :
+            Bodycare = int(input(f'Silahkan Pilih Treatment 1-{len(list_Bodycare)} '))
+            if Bodycare <= 0 or Bodycare > len(list_Bodycare) :
+                print("====================")
+                for Treatment_Bodycare in range(0, len(list_Bodycare)) :
+                    print(f'{Treatment_Bodycare + 1}. {list_Bodycare[Treatment_Bodycare]} ')
                 continue
+            else:
+                nota.append(list_Bodycare[Bodycare - 1])
+                print('==Treatment Anda==')
+                for list_nota in range(0, len(nota)):
+                    print(f'{list_nota + 1}. {nota[list_nota]}')
+                ulangi = False
+            continue
     elif Treatment == '3' :
-        for Haircare in range(0, len(list_Haircare)) :
+        for Treatment_Haircare in range(0, len(list_Haircare)) :
             print(f'{Treatment_Haircare + 1}. {list_Haircare[Treatment_Haircare]}')
         ulangi = True
         while ulangi :
@@ -55,11 +55,16 @@ while True:
                 print("====================")
                 for Treatment_Haircare in range(0, len(list_Haircare)) :
                     print(f'{Treatment_Haircare + 1}. {list_Haircare[Treatment_Haircare]}')
-                    ulangi = False
-                continue
+                ulangi = False
+            else:
+                nota.append(list_Haircare[Haircare - 1])
+                print('==Treatment Anda==')
+                for list_nota in range(0, len(nota)):
+                    print(f'{list_nota +1} . {nota[list_nota]}')
+                ulangi = False
+            continue
     else:
-        print
-        ('Treatment tidak terdaftar')
+        print('Treatment tidak terdaftar')
         continue
     lanjut = input('Apakah Ada Tambahan Treatment?')
     if lanjut == 'y' :
